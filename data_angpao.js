7c02f42c5fde61104e5ba4ae65fc51afe6717f6b
roziq <bauroziq@gmail.com>
2022-03-10 15:01:51 +0700

    first commit

---
+++ data_angpao.js
@@ -0,0 +1,39 @@
+let data = [
+    {
+        id: 0,
+        value: 20000000,
+        text: "1",
+        isShow: false,
+        agent: null,
+    },
+    {
+        id: 1,
+        value: 40000000,
+        text: "2",
+        isShow: false,
+        agent: null,
+    },
+    {
+        id: 2,
+        value: 50000000,
+        text: "3",
+        isShow: false,
+        agent: null,
+    },
+    {
+        id: 3,
+        value: 80000000,
+        text: "4",
+        isShow: false,
+        agent: null,
+    },
+    {
+        id: 4,
+        value: 10000000,
+        text: "5",
+        isShow: false,
+        agent: null,
+    },
+]
+
+export default data
